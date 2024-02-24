f7df8fe76043ee2ddc8c48d33a4593ae543ef055
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2024-02-24 14:38:35 -0500

    Initial Commit

---
+++ screamtime/screamtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  screamtimeApp.swift
+//  screamtime
+//
+//  Created by Eugene Vinitsky on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct screamtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
